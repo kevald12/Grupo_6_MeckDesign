052ba6b9a2229fbb66fb31c92a2f52eaa01dd786
CataQu <cataquarleri@gmail.com>
2021-12-05 12:14:38 -0600

    Agregamos estructura de trabajo y aplicacion Node.js+Express

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const publicPath = path.join(__dirname, '/public')
+app.use(express.static(publicPath))
+
+app.listen(4000, () => {
+    console.log('Servidor corriendo en puerto 4000')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+});
+
+app.get('/product', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'))
+});
+
+app.get('/cart', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productCart.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+});
