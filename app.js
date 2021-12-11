8193b36adf75dae451bea83197942d68c2edbe5b
Matias <matias@MBP-de-Matias.fibertel.com.ar>
2021-12-11 08:13:58 -0300

    Header - ProductDetails cambios

--- app.js
+++ app.js
@@ -5,27 +5,18 @@ const path = require('path');
 const publicPath = path.join(__dirname, '/public')
 app.use(express.static(publicPath));
 
-<<<<<<< HEAD
 app.listen(4000, () => {
     console.log('Servidor corriendo en puerto 4000')
 })
-=======
-app.listen(4000, () =>  console.log('Servidor corriendo en puerto 4000') )
-
->>>>>>> 5aa331dc7441528418f707de267d099eac252cae
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
 });
 
-<<<<<<< HEAD
 app.get('/footer', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/indexFooter.html'))
 });
 
-app.get('/product', (req, res) => {
-=======
 app.get('/productDetail', (req, res) => {
->>>>>>> 5aa331dc7441528418f707de267d099eac252cae
     res.sendFile(path.join(__dirname, '/views/productDetail.html'))
 });
 
